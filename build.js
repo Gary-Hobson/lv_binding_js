1352d3d9894a34d7b821f5e3b7b902faa5ce2602
garyhobson <garyhobson@qq.com>
2023-10-08 02:54:20 +0800

    feat: 支持命令行传递参数参数
    Signed-off-by: garyhobson <garyhobson@qq.com>

--- build.js
+++ build.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const alias = require('esbuild-plugin-alias');
 
-const entry = path.resolve(__dirname, './test/animate/2/index.jsx')
+// 检查命令行参数是否提供
+if (process.argv.length < 3) {
+    console.error("请提供要转换的文件路径");
+    process.exit(1);
+}
+
+const entry = path.resolve(process.argv[2]); // 使用第一个命令行参数作为入口文件
 
 require('esbuild').build({
     entryPoints: [entry],
     bundle: true,
     platform: 'neutral',
     external: ['path', 'fs'],
-    outfile: path.resolve(path.dirname(entry), 'index.js'),
+    outfile: path.resolve(path.dirname(entry), 'output.js'), // 输出到同一目录，命名为output.js
     plugins: [
       alias({
         'lvgljs-ui': path.resolve(__dirname, './src/render/react/index.js'),
